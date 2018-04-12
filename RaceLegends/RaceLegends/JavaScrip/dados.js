8d70577821961929d47060cc49ab81fd0ed6b936
Edgar Oliveira <aluno19987@ipt.pt>
2018-04-12 12:51:28 +0100

    Criado o ecrã que mosta as categorias sem css

--- RaceLegends/RaceLegends/JavaScrip/dados.js
+++ RaceLegends/RaceLegends/JavaScrip/dados.js
@@ -1,1 +1,21 @@
-﻿
+﻿function getCategorias() {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories";
+
+    return fetch(url, { headers: { Accept: 'application/json' } })
+        .then(function (resposta) {
+            if (resposta.status === 200) {
+                return resposta.json();
+            } else {
+                return Promise.reject(new Error("Erro ao obter categorias"));
+            }
+        });
+}
+
+function getImage(categoria) {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories/";
+    console.log(categoria);
+    url += categoria;
+    url += "/image";
+    return url;
+}
+
--- RaceLegends/RaceLegends/JavaScrip/logica.js
+++ RaceLegends/RaceLegends/JavaScrip/logica.js
@@ -1,1 +1,41 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded',function main(){
+    ecraCategorias()
+    
+})
+
+function mostraCategorias(categorias) {
+    for (var i = 0; i < categorias.length; i++) {
+        var categoria = categorias[i];
+        var CatContainer = document.createElement('div');
+        CatContainer.setAttribute("id", categoria.id )
+
+        var nomeContainer = document.createElement('h3');
+        nomeContainer.textContent = categoria.name;
+
+        var idcategoria = categoria.id;
+        var image = getImage(idcategoria);
+        var imageContainer = document.createElement('img')
+        imageContainer.setAttribute("src", image)
+
+        var descContainer = document.createElement('div');
+        descContainer.textContent = categoria.description;
+
+        CatContainer.appendChild(nomeContainer);
+        CatContainer.appendChild(imageContainer);
+        CatContainer.appendChild(descContainer);
+        
+
+        categ.appendChild(CatContainer);
+    }
+}
+    
+function ecraCategorias() {
+    return getCategorias()
+        .then(function (categorias) {
+            mostraCategorias(categorias);
+        })
+        .catch(function (erro) {
+            console.error(erro);
+        });
+}
+
